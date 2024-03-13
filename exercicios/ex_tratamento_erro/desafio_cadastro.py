8c9b823ecca07ccdf486cf9bf4740a942ca56e14
Renato Soares <renatosilsoa@gmail.com>
2024-03-13 16:58:52 -0300

    continuação no desafio

--- exercicios/ex_tratamento_erro/desafio_cadastro.py
+++ exercicios/ex_tratamento_erro/desafio_cadastro.py
@@ -2,6 +2,10 @@
 # A partir dessas informações, o sistema mostrará o nome do usuário e a idade que completou, ou completará, no ano atual (2022).
 
 # Caso o usuário não digite um número ou apareça um inválido no campo do ano, o sistema informará o erro e continuará perguntando até que um valor correto seja preenchido.
+from datetime import date
+data_atual = date.today()
+ano_atual = '{}'.format(data_atual.year)
+
 valorCorreto = False
 
 print("Cadastro")
@@ -10,13 +14,16 @@ print("Digite o seu nome completo :")
 nome_completo = input()
 
 while valorCorreto ==False:
-    print("informe o ano ")
+    print("informe o ano de nascimento")
     try:
-        ano = input()
-        if(ano < 0) or (ano.isdigit()== False):
-             raise Exception("Não digitou valor númerico positivo")
+        ano = str(input())
+        if (ano.isdigit()== ano_atual):
+            raise Exception("Por favor digite um valor númerico, ano da data do nascimento")
+        elif(ano < 1992) or (ano > data_atual):
+            raise Exception("Por favor digite um valor númerico entre 1922 e 2021")
         else: 
-            idade =            
-            print()
-
-falta concluir
+           valorCorreto = True
+           idade_atual = ano_atual-ano
+           print("Sua idade e saida", idade_atual)
+    except Exception as erro:
+        print(erro)
